0f880370b189f0bcb5e9e2f4936a1b71dbfe9992
Laszlo Tamas <laszlo.devil@gmail.com>
2016-07-29 16:05:27 +0200

    Calculation part

--- ZPL_UTF8_test/Form1.Designer.vb
+++ ZPL_UTF8_test/Form1.Designer.vb
@@ -26,7 +26,25 @@ Partial Class Form1
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.TableLayoutPanel2 = New System.Windows.Forms.TableLayoutPanel()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TextBoxResDpmm = New System.Windows.Forms.TextBox()
+        Me.TextBoxResmm = New System.Windows.Forms.TextBox()
+        Me.TableLayoutPanel3 = New System.Windows.Forms.TableLayoutPanel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBoxResHanymm = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TableLayoutPanel4 = New System.Windows.Forms.TableLayoutPanel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.TextBoxResLabelW = New System.Windows.Forms.TextBox()
+        Me.TextBoxResLabelH = New System.Windows.Forms.TextBox()
         Me.TableLayoutPanel1.SuspendLayout()
+        Me.TableLayoutPanel2.SuspendLayout()
+        Me.TableLayoutPanel3.SuspendLayout()
+        Me.TableLayoutPanel4.SuspendLayout()
         Me.SuspendLayout()
         '
         'TableLayoutPanel1
@@ -37,12 +55,19 @@ Partial Class Form1
         Me.TableLayoutPanel1.Controls.Add(Me.TextBox1, 0, 0)
         Me.TableLayoutPanel1.Controls.Add(Me.TextBox2, 0, 1)
         Me.TableLayoutPanel1.Controls.Add(Me.Button1, 1, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.TableLayoutPanel2, 0, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.TableLayoutPanel3, 0, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button2, 1, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.TableLayoutPanel4, 0, 4)
         Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
         Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
-        Me.TableLayoutPanel1.RowCount = 2
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.RowCount = 5
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 35.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 35.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
         Me.TableLayoutPanel1.Size = New System.Drawing.Size(670, 302)
         Me.TableLayoutPanel1.TabIndex = 0
         '
@@ -53,17 +78,17 @@ Partial Class Form1
         Me.TextBox1.Multiline = True
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
-        Me.TextBox1.Size = New System.Drawing.Size(563, 145)
+        Me.TextBox1.Size = New System.Drawing.Size(563, 99)
         Me.TextBox1.TabIndex = 0
         '
         'TextBox2
         '
         Me.TextBox2.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.TextBox2.Location = New System.Drawing.Point(3, 154)
+        Me.TextBox2.Location = New System.Drawing.Point(3, 108)
         Me.TextBox2.Multiline = True
         Me.TextBox2.Name = "TextBox2"
         Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both
-        Me.TextBox2.Size = New System.Drawing.Size(563, 145)
+        Me.TextBox2.Size = New System.Drawing.Size(563, 99)
         Me.TextBox2.TabIndex = 1
         '
         'Button1
@@ -71,10 +96,186 @@ Partial Class Form1
         Me.Button1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.Button1.Location = New System.Drawing.Point(572, 3)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(95, 145)
+        Me.Button1.Size = New System.Drawing.Size(95, 99)
         Me.Button1.TabIndex = 2
         Me.Button1.Text = "Convert"
         Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TableLayoutPanel2
+        '
+        Me.TableLayoutPanel2.ColumnCount = 4
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel2.Controls.Add(Me.ComboBox1, 0, 0)
+        Me.TableLayoutPanel2.Controls.Add(Me.TextBoxResDpmm, 1, 0)
+        Me.TableLayoutPanel2.Controls.Add(Me.TextBoxResmm, 2, 0)
+        Me.TableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel2.Location = New System.Drawing.Point(3, 213)
+        Me.TableLayoutPanel2.Name = "TableLayoutPanel2"
+        Me.TableLayoutPanel2.RowCount = 1
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel2.Size = New System.Drawing.Size(563, 24)
+        Me.TableLayoutPanel2.TabIndex = 3
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.BackColor = System.Drawing.Color.Yellow
+        Me.ComboBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"203dpi", "300dpi"})
+        Me.ComboBox1.Location = New System.Drawing.Point(3, 3)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(134, 21)
+        Me.ComboBox1.TabIndex = 0
+        Me.ComboBox1.Text = "203dpi"
+        '
+        'TextBoxResDpmm
+        '
+        Me.TextBoxResDpmm.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.TextBoxResDpmm.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBoxResDpmm.Location = New System.Drawing.Point(143, 3)
+        Me.TextBoxResDpmm.Name = "TextBoxResDpmm"
+        Me.TextBoxResDpmm.Size = New System.Drawing.Size(134, 20)
+        Me.TextBoxResDpmm.TabIndex = 1
+        Me.TextBoxResDpmm.Text = "8dpmm"
+        '
+        'TextBoxResmm
+        '
+        Me.TextBoxResmm.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.TextBoxResmm.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBoxResmm.Location = New System.Drawing.Point(283, 3)
+        Me.TextBoxResmm.Name = "TextBoxResmm"
+        Me.TextBoxResmm.Size = New System.Drawing.Size(134, 20)
+        Me.TextBoxResmm.TabIndex = 2
+        Me.TextBoxResmm.Text = "8"
+        '
+        'TableLayoutPanel3
+        '
+        Me.TableLayoutPanel3.ColumnCount = 4
+        Me.TableLayoutPanel3.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel3.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel3.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel3.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel3.Controls.Add(Me.Label1, 0, 0)
+        Me.TableLayoutPanel3.Controls.Add(Me.TextBox5, 1, 0)
+        Me.TableLayoutPanel3.Controls.Add(Me.TextBoxResHanymm, 2, 0)
+        Me.TableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel3.Location = New System.Drawing.Point(3, 243)
+        Me.TableLayoutPanel3.Name = "TableLayoutPanel3"
+        Me.TableLayoutPanel3.RowCount = 1
+        Me.TableLayoutPanel3.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel3.Size = New System.Drawing.Size(563, 24)
+        Me.TableLayoutPanel3.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Label1.Location = New System.Drawing.Point(83, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(54, 24)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Hány mm:"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.BackColor = System.Drawing.Color.Yellow
+        Me.TextBox5.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox5.Location = New System.Drawing.Point(143, 3)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(134, 20)
+        Me.TextBox5.TabIndex = 1
+        Me.TextBox5.Text = "0"
+        '
+        'TextBoxResHanymm
+        '
+        Me.TextBoxResHanymm.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.TextBoxResHanymm.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBoxResHanymm.Location = New System.Drawing.Point(283, 3)
+        Me.TextBoxResHanymm.Name = "TextBoxResHanymm"
+        Me.TextBoxResHanymm.Size = New System.Drawing.Size(134, 20)
+        Me.TextBoxResHanymm.TabIndex = 2
+        '
+        'Button2
+        '
+        Me.Button2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button2.Location = New System.Drawing.Point(572, 213)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(95, 24)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Calc"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TableLayoutPanel4
+        '
+        Me.TableLayoutPanel4.ColumnCount = 5
+        Me.TableLayoutPanel4.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.0!))
+        Me.TableLayoutPanel4.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.0!))
+        Me.TableLayoutPanel4.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.0!))
+        Me.TableLayoutPanel4.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.0!))
+        Me.TableLayoutPanel4.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.0!))
+        Me.TableLayoutPanel4.Controls.Add(Me.Label2, 0, 0)
+        Me.TableLayoutPanel4.Controls.Add(Me.TextBox7, 1, 0)
+        Me.TableLayoutPanel4.Controls.Add(Me.TextBox8, 2, 0)
+        Me.TableLayoutPanel4.Controls.Add(Me.TextBoxResLabelW, 3, 0)
+        Me.TableLayoutPanel4.Controls.Add(Me.TextBoxResLabelH, 4, 0)
+        Me.TableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel4.Location = New System.Drawing.Point(3, 273)
+        Me.TableLayoutPanel4.Name = "TableLayoutPanel4"
+        Me.TableLayoutPanel4.RowCount = 1
+        Me.TableLayoutPanel4.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel4.Size = New System.Drawing.Size(563, 26)
+        Me.TableLayoutPanel4.TabIndex = 6
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Label2.Location = New System.Drawing.Point(42, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(67, 26)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Címkeméret:"
+        '
+        'TextBox7
+        '
+        Me.TextBox7.BackColor = System.Drawing.Color.Yellow
+        Me.TextBox7.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox7.Location = New System.Drawing.Point(115, 3)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(106, 20)
+        Me.TextBox7.TabIndex = 1
+        Me.TextBox7.Text = "90"
+        '
+        'TextBox8
+        '
+        Me.TextBox8.BackColor = System.Drawing.Color.Yellow
+        Me.TextBox8.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox8.Location = New System.Drawing.Point(227, 3)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(106, 20)
+        Me.TextBox8.TabIndex = 2
+        Me.TextBox8.Text = "50"
+        '
+        'TextBoxResLabelW
+        '
+        Me.TextBoxResLabelW.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.TextBoxResLabelW.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBoxResLabelW.Location = New System.Drawing.Point(339, 3)
+        Me.TextBoxResLabelW.Name = "TextBoxResLabelW"
+        Me.TextBoxResLabelW.Size = New System.Drawing.Size(106, 20)
+        Me.TextBoxResLabelW.TabIndex = 3
+        '
+        'TextBoxResLabelH
+        '
+        Me.TextBoxResLabelH.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.TextBoxResLabelH.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBoxResLabelH.Location = New System.Drawing.Point(451, 3)
+        Me.TextBoxResLabelH.Name = "TextBoxResLabelH"
+        Me.TextBoxResLabelH.Size = New System.Drawing.Size(109, 20)
+        Me.TextBoxResLabelH.TabIndex = 4
         '
         'Form1
         '
@@ -86,6 +287,12 @@ Partial Class Form1
         Me.Text = "Zebra UTF8 converter"
         Me.TableLayoutPanel1.ResumeLayout(False)
         Me.TableLayoutPanel1.PerformLayout()
+        Me.TableLayoutPanel2.ResumeLayout(False)
+        Me.TableLayoutPanel2.PerformLayout()
+        Me.TableLayoutPanel3.ResumeLayout(False)
+        Me.TableLayoutPanel3.PerformLayout()
+        Me.TableLayoutPanel4.ResumeLayout(False)
+        Me.TableLayoutPanel4.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -94,4 +301,19 @@ Partial Class Form1
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents Button1 As Button
+    Friend WithEvents TableLayoutPanel2 As TableLayoutPanel
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TextBoxResDpmm As TextBox
+    Friend WithEvents TextBoxResmm As TextBox
+    Friend WithEvents TableLayoutPanel3 As TableLayoutPanel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBoxResHanymm As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TableLayoutPanel4 As TableLayoutPanel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TextBoxResLabelW As TextBox
+    Friend WithEvents TextBoxResLabelH As TextBox
 End Class
--- ZPL_UTF8_test/Form1.vb
+++ ZPL_UTF8_test/Form1.vb
@@ -2,4 +2,24 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         TextBox2.Text = ZebraPrint.GetZPLutf8Code(TextBox1.Text)
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim dpmm As Integer
+
+        dpmm = CInt(TextBoxResmm.Text)
+        TextBoxResHanymm.Text = CStr(CLng(TextBox5.Text) * dpmm)
+        TextBoxResLabelW.Text = CStr(CLng(TextBox7.Text) * dpmm)
+        TextBoxResLabelH.Text = CStr(CLng(TextBox8.Text) * dpmm)
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        If ComboBox1.Text = "203dpi" Then
+            TextBoxResDpmm.Text = "8dpmm"
+            TextBoxResmm.Text = "8"
+        End If
+        If ComboBox1.Text = "300dpi" Then
+            TextBoxResDpmm.Text = "12dpmm"
+            TextBoxResmm.Text = "12"
+        End If
+    End Sub
 End Class
